a7815c97c27c6cd2fba0a5b8bd3d37b7e57a19dc
haraharb <haraharb@gmail.com>
2025-06-19 11:19:40 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,18 @@
-self.addEventListener('fetch', e=>e.respondWith(fetch(e.request)));
+// ---- PWA cache v2 ----
+const CACHE = 'sqborder-v2';
+const ASSETS = [
+  './',              // ルート
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
